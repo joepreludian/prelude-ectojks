77f29a5dc91d80b242adff809866952e026524d8
Jonhnatha Trigueiro <joepreludian@gmail.com>
2021-01-30 17:52:53 -0300

    Initial commit

---
+++ vars/prlBuildPoetryPipeline.groovy
@@ -0,0 +1,23 @@
+/*
+ * Pipeline for Poetry build
+ */
+
+def setBuildDetails() {
+    currentBuild.rawBuild.project.description = '<h3>Python Poetry</h3><p>Temp</p>'
+}
+
+def call(Map conf = [:]) {
+
+    pipeline {
+        agent any
+            stages {
+                stage('Testing') {
+                    steps {
+                        setBuildDetails()
+                    }
+                }
+            }
+    }
+}
+
+}
