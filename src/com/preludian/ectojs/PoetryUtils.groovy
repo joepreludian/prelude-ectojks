8fbf8e9f9c08a6cd824195ac10f99ec650ba0862
Jonhnatha Trigueiro <joepreludian@gmail.com>
2021-02-01 02:33:05 -0300

    Add new functionality

---
+++ src/com/preludian/ectojs/PoetryUtils.groovy
@@ -0,0 +1,21 @@
+@Grab('org.tomlj:tomlj:1.0.0')
+
+import org.tomlj.*
+import java.nio.file.Paths
+import java.nio.file.Path
+
+
+def getInfo() {
+
+    Path source = Paths.get("./pyproject.toml");
+
+    TomlParseResult result = Toml.parse(source);
+    result.errors().forEach(error -> System.err.println(error.toString()));
+
+    String value = result.getString("tool.poetry.version")
+    return [
+            version: result.getString("tool.poetry.version")
+    ]
+}
+
+return this
